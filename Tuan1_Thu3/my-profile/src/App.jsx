744ab66fe951f16efb29629059f301e8047de35a
vovantri123 <vovantri204@gmail.com>
2025-08-19 21:56:42 +0700

    My Profile with ReactJS

---
+++ Tuan1_Thu3/my-profile/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <> 
+      <div className="card" style={{width:"800px"}}>
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+          <h1>Thông tin cá nhân</h1>
+          <p>Họ và tên: Võ Văn Trí</p>
+          <p>MSSV: 22110444</p> 
+        </div>
+      </div> 
+    </>
+  )
+}
+
+export default App
