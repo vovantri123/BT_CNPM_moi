33fb7180d81f57f51798f7b766e40ddfcc3d7847
vovantri123 <vovantri204@gmail.com>
2025-09-03 10:38:36 +0700

    Add static file serving (img,css)

--- Tuan2_Thu4/FullStackNodeJS01/ExpressJS01/src/config/viewEngine.js
+++ Tuan2_Thu4/FullStackNodeJS01/ExpressJS01/src/config/viewEngine.js
@@ -2,11 +2,13 @@ const path = require('path');
 const express = require('express');
 
 const configViewEngine = (app) => {
-    app.set('views', path.join('./src/', 'views'));
-    app.set('view engine', 'ejs');
-    
-    // config static files
-    app.use(express.static(path.join('./src/', 'public')));
+    app.set("views", path.join('./src', 'views')); // cho Express biết thư mục chứa các file view template (ở đây dùng EJS).
+    app.set("view engine", "ejs"); // Đặt view engine mặc định là ejs, Sau này khi bạn gọi res.render("home"), Express sẽ tự tìm file home.ejs trong thư mục views.
+
+  
+    app.use(express.static(path.join('./src','public'))); // middleware để phục vụ file tĩnh (static files) như CSS, JS, ảnh, fonts...
+    // Khi này http://localhost:8080 tương ứng với ./src/public
+    // Nghĩa là khi bạn truy cập http://localhost:8080/img/logo.png, Express sẽ tự tìm file ./src/public/img/logo.png để trả về.
 }
 
 module.exports = configViewEngine;
